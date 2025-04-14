bd33850ae6f35518da566befdf57ae3f66b42d92
Lailson Santana <lailsonbit@gmail.com>
2025-04-14 13:22:11 -0300

    Inicialização do Projeto

---
+++ src/test/java/com/example/rankify/RankifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rankify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
